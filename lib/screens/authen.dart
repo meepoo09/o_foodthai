89b2a639c61f6f06a8e1bf8a0abb97ba137bf780
meepoo09 <meepoo09@hotmail.com>
2019-07-15 14:05:13 +0700

    ui ok

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -9,6 +9,78 @@ class _AuthenState extends State<Authen> {
 //  Explicit
 
 // medthod
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.orange[300],
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      color: Colors.blue[300],
+      child: Text(
+        'Sign Up',
+        style: TextStyle(color: Colors.orange[300]),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: signInButton(),
+          ),
+          mySizeBox(),
+          Expanded(
+            child: signUpButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget userText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'User :',
+          labelStyle: TextStyle(color: Colors.orangeAccent),
+          hintText: 'Not Blank',
+        ),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Password :',
+          labelStyle: TextStyle(color: Colors.orangeAccent),
+          hintText: 'Not Blank',
+        ),
+      ),
+    );
+  }
+
+  Widget mySizeBox() {
+    return SizedBox(
+      height: 16.0,
+      width: 8.0,
+    );
+  }
+
   Widget showLogo() {
     return Container(
         width: 160.0, height: 160.0, child: Image.asset('images/logo.png'));
@@ -16,10 +88,11 @@ class _AuthenState extends State<Authen> {
 
   Widget showAppName() {
     return Text(
-      'Fodddd',
+      'โอเบค',
       style: TextStyle(
         fontSize: 36.0,
-        fontWeight: FontWeight.bold,color: Colors.orange[200],
+        fontWeight: FontWeight.bold,
+        color: Colors.orange[200],
         fontFamily: 'Prompt',
       ),
     );
@@ -28,11 +101,26 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.teal[100]],
+            begin: Alignment.topCenter,
+          ),
+        ),
         padding: EdgeInsets.only(top: 50.0),
         alignment: Alignment.topCenter,
         child: Column(
-          children: <Widget>[showLogo(), showAppName()],
+          children: <Widget>[
+            showLogo(),
+            mySizeBox(),
+            showAppName(),
+            userText(),
+            passwordText(),
+            mySizeBox(),
+            showButton(),
+          ],
         ),
       ),
     );
