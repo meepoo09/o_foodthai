5c5230b550f96a10d4d8a643c2fd1d93e8a3bf54
meepoo09 <meepoo09@hotmail.com>
2019-07-16 10:40:50 +0700

    http

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
 
 class Register extends StatefulWidget {
   @override
@@ -28,59 +30,61 @@ class _RegisterState extends State<Register> {
         if (value.isEmpty) {
           return 'Please Fill Name In Blank';
         }
-      },onSaved: (String value){
-        name=value;
-
+      },
+      onSaved: (String value) {
+        name = value;
       },
     );
   }
 
   Widget userText() {
     return TextFormField(
-        decoration: InputDecoration(
-          icon: Icon(
-            Icons.account_circle,
-            size: 36.0,
-            color: Colors.orange[300],
-          ),
-          labelText: 'User :',
-          labelStyle: TextStyle(color: Colors.orange[300]),
-          helperText: 'Type UserName',
-          helperStyle:
-              TextStyle(color: Colors.black26, fontStyle: FontStyle.italic),
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.account_circle,
+          size: 36.0,
+          color: Colors.orange[300],
         ),
-        validator: (String value) {
-          if (value.isEmpty) {
-            return 'Please Fill Name In Blank';
-          }
-        },onSaved: (String value){
-        user=value;
-        
-      },);
+        labelText: 'User :',
+        labelStyle: TextStyle(color: Colors.orange[300]),
+        helperText: 'Type UserName',
+        helperStyle:
+            TextStyle(color: Colors.black26, fontStyle: FontStyle.italic),
+      ),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Please Fill Name In Blank';
+        }
+      },
+      onSaved: (String value) {
+        user = value;
+      },
+    );
   }
 
   Widget passwordText() {
     return TextFormField(
-        decoration: InputDecoration(
-          icon: Icon(
-            Icons.lock,
-            size: 36.0,
-            color: Colors.orange[300],
-          ),
-          labelText: 'Password :',
-          labelStyle: TextStyle(color: Colors.orange[300]),
-          helperText: 'Type Password',
-          helperStyle:
-              TextStyle(color: Colors.black26, fontStyle: FontStyle.italic),
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+          color: Colors.orange[300],
         ),
-        validator: (String value) {
-          if (value.isEmpty) {
-            return 'Please Fill Name In Blank';
-          }
-        },onSaved: (String value){
-        password=value;
-        
-      },);
+        labelText: 'Password :',
+        labelStyle: TextStyle(color: Colors.orange[300]),
+        helperText: 'Type Password',
+        helperStyle:
+            TextStyle(color: Colors.black26, fontStyle: FontStyle.italic),
+      ),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Please Fill Name In Blank';
+        }
+      },
+      onSaved: (String value) {
+        password = value;
+      },
+    );
   }
 
   Widget uploadButton() {
@@ -91,9 +95,21 @@ class _RegisterState extends State<Register> {
         if (formkey.currentState.validate()) {
           formkey.currentState.save();
           print('name=$name, user = $user, password=$password');
+          registerToServer();
         }
       },
     );
+  }
+
+  Future<void> registerToServer() async {
+    String url =
+        'https://www.androidthai.in.th/ooo/addDataO.php?isAdd=true&Name=$name&User=$user&Password=$password';
+    var response = await get(url);
+    var result = json.decode(response.body);
+    print('result = $result');
+    if ((result.toString())=='true') {
+      Navigator.of(context).pop();
+    }
   }
 
   @override
